4bc90cd3ab2a6bee634dcccc70f9dec7d5c92523
Wanpan <wanpan96@163.com>
2022-01-26 12:46:48 +0800

    fix: path __dirname

--- cli/getTemplate.js
+++ cli/getTemplate.js
@@ -4,24 +4,29 @@ const path = require("path");
 const ora = require("ora");
 
 function getTemplate({ projectName }) {
-  const spinner = ora("Loading unicorns");
+  const spinner = ora("模板创建中");
+  spinner.color = "yellow";
   spinner.start();
 
-  const sourceDir = path.resolve(process.cwd(), `./template`);
-  const projectDir = path.resolve(process.cwd(), `./${projectName}`);
+  try {
+    const sourceDir = path.resolve(__dirname, `../template`);
+    const projectDir = path.resolve(process.cwd(), `./${projectName}`);
 
-  // 复制模板
-  fs.copy(sourceDir, projectDir, () => {
+    fs.copy(sourceDir, projectDir, () => {
+      spinner.stop();
+      // 修改项目名称
+      const packageObj = fs.readJsonSync(`${projectDir}/package.json`);
+      packageObj.name = projectName;
+      fs.outputFileSync(
+        `${projectDir}/package.json`,
+        JSON.stringify(packageObj, "", "\t")
+      );
+      console.log("模板创建完成");
+    });
+  } catch (error) {
     spinner.stop();
-    // 修改项目名称
-    const packageObj = fs.readJsonSync(`${projectDir}/package.json`);
-    packageObj.name = projectName;
-    fs.outputFileSync(
-      `${projectDir}/package.json`,
-      JSON.stringify(packageObj, "", "\t")
-    );
-    console.log("模板创建完成");
-  });
+    console.log(error);
+  }
 }
 
 exports.getTemplate = getTemplate;
