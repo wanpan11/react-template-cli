0cf0e08d00bdb0166de14d2cb25decd15b78d164
wanpan <wanpan96@163.com>
2023-02-27 15:42:24 +0800

    cli update check

--- cli/index.js
+++ cli/index.js
@@ -2,35 +2,50 @@ const program = require("commander");
 const inquirer = require("inquirer");
 const info = require("../package.json");
 const { getTemplate } = require("./getTemplate");
+const updater = require("pkg-updater");
 
-program.version(info.version, "-v, --version");
-program.description("react 项目模板初始化工具");
+const updaterMsg = `package update from <%=current%> to <%=latest%>.
+run 'npm i ${info.name} -g'
+`;
 
-program
-  .usage("<command>")
-  .command("init")
-  .usage(" ")
-  .description("创建 react 项目模板")
-  .action(() => {
-    const prompt = [
-      {
-        type: "input",
-        message: "请输入项目名称",
-        name: "projectName",
-        default: "react-app",
-      },
-      {
-        type: "list",
-        message: "请选择模板类型：",
-        name: "type",
-        default: "react",
-        choices: ["react", "react-ts"],
-      },
-    ];
+updater({
+  pkg: info,
+  registry: `https://registry.npmjs.org/${info.name}`, // custom registry
+  tag: "next", // custom the check tag(default is latest)
+  checkInterval: 24 * 60 * 60 * 1000, // custom the check interval(ms)
+  updateMessage: updaterMsg, // custom notify message
+}).then(() => {
+  /* start cli here */
 
-    inquirer.prompt(prompt).then(answers => {
-      getTemplate(answers);
+  program.version(info.version, "-v, --version");
+  program.description("react 项目模板初始化工具");
+
+  program
+    .usage("<command>")
+    .command("init")
+    .usage(" ")
+    .description("创建 react 项目模板")
+    .action(() => {
+      const prompt = [
+        {
+          type: "input",
+          message: "请输入项目名称",
+          name: "projectName",
+          default: "react-app",
+        },
+        {
+          type: "list",
+          message: "请选择模板类型：",
+          name: "type",
+          default: "react",
+          choices: ["react", "react-ts"],
+        },
+      ];
+
+      inquirer.prompt(prompt).then(answers => {
+        getTemplate(answers);
+      });
     });
-  });
 
-program.parse(process.argv);
+  program.parse(process.argv);
+});
